d711943fbb387ae7c2d64c2015604397f5933574
Kishan Raja <kishan.raja96@gmail.com>
2022-03-01 16:32:39 -0500

    CRD working on BE

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,6 +3,14 @@ const router = express.Router()
 const Posts = require('../models/posts.js')
 
 
+
+router.delete('/:id', (req,res) => {
+  Posts.findByIdAndRemove(req.params.id, (err, deletedPost) => {
+    res.json(deletedPost)
+  })
+})
+
+
 router.post('/', (req,res) => {
   Posts.create(req.body, (err, createdPost) => {
     res.json(createdPost)
