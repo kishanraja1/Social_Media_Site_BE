be0d8c52b4f83058b283c2627712831e81faf823
Darwood Antoine <darwoodantoine@Darwoods-MacBook-Pro.local>
2022-03-04 10:08:17 -0500

    added to schema

--- models/posts.js
+++ models/posts.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
   name: String,
+  author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   post: String,
   checking: Boolean,
   likes: Number,
   dislikes: Number,
   likeBoolean: Boolean,
-  dislikeBoolean:Boolean
-})
-
+  dislikeBoolean:Boolean,
+},{timestamps:true})
 
 const Posts = mongoose.model('Post', postSchema)
 
