c6f0a97ce34c9327b30eabf2372780605470c8a9
Kishan Raja <kishan.raja96@gmail.com>
2022-03-07 11:35:16 -0500

    post name and date

--- models/posts.js
+++ models/posts.js
@@ -7,8 +7,6 @@ const postSchema = new mongoose.Schema({
   checking: Boolean,
   likes: Number,
   dislikes: Number,
-  likeBoolean: Boolean,
-  dislikeBoolean:Boolean,
   comments: [{body: String}],
   date: {type: Date, default: Date.now},
   react:[{rAuthor: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true, default:"622155e2385f63508588e9a0"},
